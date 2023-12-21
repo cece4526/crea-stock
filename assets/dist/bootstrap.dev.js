724ae686ba9722fbb7e239536a10eb5f4bd8af43
cece4526 <cece4526@hotmail.fr>
2023-12-21 16:21:21 +0100

    commit asset

---
+++ assets/dist/bootstrap.dev.js
@@ -0,0 +1,6 @@
+"use strict";
+
+var _stimulusBundle = require("@symfony/stimulus-bundle");
+
+var app = (0, _stimulusBundle.startStimulusApp)(); // register any custom, 3rd party controllers here
+// app.register('some_controller_name', SomeImportedController);
